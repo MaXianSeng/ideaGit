4e58a3f3e7106512b134cba33bd105f9673b73e2
MaXianSeng <197744853@qq.com>
2022-02-17 16:10:20 +0800

    GitDemo.java的第三次commit

--- src/main/java/edn/shu/GitDemo.java
+++ src/main/java/edn/shu/GitDemo.java
@@ -9,5 +9,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("第二次修改");
+        System.out.println("第三次修改");
     }
 }
