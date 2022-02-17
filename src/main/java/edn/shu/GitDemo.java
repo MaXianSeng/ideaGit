3a6bd1df21062de90fd5cfc2f9b0efbbbabb5fcc
MaXianSeng <197744853@qq.com>
2022-02-17 16:02:52 +0800

    第一次向本地仓库提交文件

---
+++ src/main/java/edn/shu/GitDemo.java
@@ -0,0 +1,12 @@
+package edn.shu;
+
+/**
+ * @Description: 学习idea中的git插件所新建的类
+ * @Auther: Xiandong Ma
+ * @Date: 2022/2/17 10:55
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
